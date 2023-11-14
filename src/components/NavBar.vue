d0ca96049c819a5b7955179f5f763b4dde925dd2
Khari <khariwoods3@gmail.com>
2023-11-14 15:52:31 -0400

    Inital commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,17 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom">
+        <div class="container py-2">
+            <a href="#" class="navbar-brand">
+            <span>Diary</span>
+            <strong> Cloud</strong>
+            </a>
+        </div>
+
+        <!-- <input type="" name="" value=""> -->
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    </nav>
+    
+</template>
